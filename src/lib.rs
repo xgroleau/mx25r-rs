e39909b04a30b40a3d7b0a4c5ee106e732488e20
xgroleau🐢 <xavgroleau@gmail.com>
2022-04-12 11:49:22 -0400

    removed flake from cargo publish

--- src/lib.rs
+++ src/lib.rs
@@ -16,12 +16,3 @@ pub mod blocking;
 mod command;
 pub mod error;
 pub mod register;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
